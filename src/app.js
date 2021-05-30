0980a0140301c76b2eb39ff712bb2963ce196f39
Firas Hasan <x.firashasan@gmail.com>
2021-05-30 19:16:59 +0300

    lab7 is working

--- src/app.js
+++ src/app.js
@@ -1,34 +1,42 @@
 import React from 'react';
-import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
+import Header from './header';
+// import IsLoadingAndError from './IsLoadingAndError';
+import Footer from './footer';
 import {
   BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
-
+import Login from './login';
+import Logout from './logOutButton';
+import User from './user';
+import { withAuth0 } from '@auth0/auth0-react';
+//it only exports the app if it's authorized
 class App extends React.Component {
 
   render() {
-    console.log('app', this.props)
+    console.log('app', this.props.auth0.isAuthenticated);
     return(
       <>
         <Router>
-          <IsLoadingAndError>
+          {/* <IsLoadingAndError> */}
             <Header />
               <Switch>
                 <Route exact path="/">
                   {/* TODO: if the user is logged in, render the `MyFavoriteBooks` component, if they are not, render the `Login` component */}
+                  {!this.props.auth0.isAuthenticated?<Login></Login>:<Logout></Logout>}
                 </Route>
                 {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+                <Route exact path="/profile">
+                  <User></User>
+                  </Route>
               </Switch>
             <Footer />
-          </IsLoadingAndError>
+          {/* </IsLoadingAndError> */}
         </Router>
       </>
     )
   }
 }
 
-export default App;
+export default withAuth0(App);
